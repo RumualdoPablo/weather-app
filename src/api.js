d51363d726c2a4e071a0dfa92fa286086dc8e424
Pablo Rumualdo <pablorumualdo2305@gmail.com>
2022-07-28 20:10:25 -0300

    Complete weather App

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c3a5ada69amshe68b0dfadc0b2bbp1b5f49jsn8497739b7ef8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "07840b7298d5a4d65727ac69872ac61b";
